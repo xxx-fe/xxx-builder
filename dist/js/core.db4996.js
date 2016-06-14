b12414e2629a0e45f59f10d6c436b54ec170db43
subying <subying@subyings-MacBook-Pro.local>
2016-06-14 23:01:31 +0800

    up

--- dist/js/core.db4996.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2,6],{0:function(n,t,c){n.exports=c(12)},12:function(n,t){"use strict"}});
-//# sourceMappingURL=core.db4996.js.map
--- dist/js/test_b.db4996.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5,6],{0:function(n,t,o){n.exports=o(17)},10:function(n,t,o){n.exports=o.p+"img/404.8801de.png"},17:function(n,t,o){"use strict";o(10)}});
-//# sourceMappingURL=test_b.db4996.js.map
